0f581326606bfc1949f8f282ac6a254bfd24d340
Kavita <kkavita92@gmail.com>
2017-07-11 07:45:40 +0100

    Refactor feature test into methods

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,10 @@
 require '././lib/account'
 
-account = Account.new
-account.make_deposit(1000)
-account.make_withdrawal(500.50)
-account.get_statement
+def test_standard_account_activity()
+  account = Account.new
+  account.make_deposit(1000)
+  account.make_withdrawal(500.50)
+  account.get_statement
+end
+
+test_standard_account_activity()
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,7 +2,16 @@ require 'statement'
 
 describe Statement do
   subject(:statement) { described_class.new }
+  
+  describe '#print' do
+    it 'should output multiple transactions' do
+    end
 
-  it ''
+    it 'should output one transaction' do
+    end
+
+    it 'should output transactions in reverse order' do
+    end
+  end
 
 end
